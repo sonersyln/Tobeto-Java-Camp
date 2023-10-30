26b3c5a14a87249cddd45c47025fb8d1e13888ea
Soner <syln.soner@gmail.com>
2023-10-30 18:47:58 +0300

    Fibonacci Series

--- workshop_30.10.23/src/main/java/org/example/FibonacciSeries.java
+++ workshop_30.10.23/src/main/java/org/example/FibonacciSeries.java
@@ -1,5 +1,19 @@
 package org.example;
 
 public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 100;
+        long a = 0, b = 1;
 
+        System.out.println("1. Fibonacci Number: " + a);
+        System.out.println("2. Fibonacci Number: " + b);
+
+        for (int i = 2; i < n; i++) {
+            long next = a + b;
+            System.out.println((i + 1) + ". Fibonacci Number: " + next);
+            a = b;
+            b = next;
+        }
+    }
 }
+
