b201061fab995b09ce862caf80ed908b1b79b003
Soner <syln.soner@gmail.com>
2023-10-31 23:48:54 +0300

    Perfect Number workshop

--- workshop_30.10.23/src/main/java/org/example/PerfectNumber.java
+++ workshop_30.10.23/src/main/java/org/example/PerfectNumber.java
@@ -2,26 +2,34 @@ package org.example;
 
 public class PerfectNumber {
     public static void main(String[] args) {
+        int count = 0; // Bulunan mükemmel sayıların sayısı
+        long number = 2; // Formülde kullanılan sayının başlangıç değeri
 
-        int num = 1;
-        int perfectCount = 0;
+        System.out.println(" 10 Perfect Number :");
 
-        while (perfectCount < 10) {
+        while (count < 10) {
+            long perfect = ((1L << number) - 1) * (1L << (number - 1));
 
-            int sum = 0;
+            if (isPerfect(perfect)) {
+                System.out.println(perfect);
+                count++;
+            }
+            number++;
+        }
+    }
 
-            for (int i = 1; i <= num / 2; i++) {
-                if (num % i == 0) {
-                    sum += i;
+    // Bir sayının mükemmel olup olmadığını kontrol eden fonksiyon
+    public static boolean isPerfect(long n) {
+        long sum = 1; // 1 her zaman bölen olarak kullanılır
+        for (long i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                sum += i;
+                if (i != n / i) {
+                    sum += n / i;
                 }
             }
-
-            if (sum == num) {
-                System.out.println(num + " is a perfect number");
-                perfectCount++;
-            }
-            num++;
         }
+        return sum == n;
     }
 }
 
