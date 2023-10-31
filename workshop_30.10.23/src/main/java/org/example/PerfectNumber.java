e900973ed0324490bb3c614d071b551f77ab2b9e
Soner <syln.soner@gmail.com>
2023-10-31 23:56:10 +0300

    Perfect Number workshop v1

--- workshop_30.10.23/src/main/java/org/example/PerfectNumber.java
+++ workshop_30.10.23/src/main/java/org/example/PerfectNumber.java
@@ -2,25 +2,26 @@ package org.example;
 
 public class PerfectNumber {
     public static void main(String[] args) {
-        int count = 0; // Bulunan mükemmel sayıların sayısı
-        long number = 2; // Formülde kullanılan sayının başlangıç değeri
+        int count = 0; // The count of found perfect numbers
+        long number = 2; // The starting value of the number used in the formula
 
-        System.out.println(" 10 Perfect Number :");
+        System.out.println("10 Perfect Numbers:");
 
         while (count < 10) {
+            // Calculate the perfect number
             long perfect = ((1L << number) - 1) * (1L << (number - 1));
 
-            if (isPerfect(perfect)) {
-                System.out.println(perfect);
+            if (isPerfect(perfect)) { // Check if it's a perfect number
+                System.out.println(perfect); // Print the perfect number
                 count++;
             }
             number++;
         }
     }
 
-    // Bir sayının mükemmel olup olmadığını kontrol eden fonksiyon
+    // A function to check whether a number is perfect
     public static boolean isPerfect(long n) {
-        long sum = 1; // 1 her zaman bölen olarak kullanılır
+        long sum = 1; // 1 is always used as a divisor
         for (long i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 sum += i;
@@ -29,7 +30,6 @@ public class PerfectNumber {
                 }
             }
         }
-        return sum == n;
+        return sum == n; // Is the sum of divisors equal to the original number?
     }
 }
-
